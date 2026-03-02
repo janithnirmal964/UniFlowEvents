2f9c6a3eaddd6613a54dffa77b9fa568421d584d
Janith854 <janithnirmal29@gmail.com>
2026-03-02 16:37:35 +0530

    Setup: Initial folder structure for UniFlowEvents

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('UniFlowEvents API is running...');
+});
+
+module.exports = app;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,1 @@
+// TODO: Implement Register
